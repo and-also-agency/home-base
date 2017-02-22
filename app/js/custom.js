e51cc156235ee0c51005aa37f01a5afd98a02c52
Sam Graim <sam@andalsoagency.com>
2017-02-21 21:27:06 -0700

    Homepage and about page updates.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,15 +1,16 @@
 // Nav //
 // Wait for the DOM to be ready (all elements printed on page regardless if loaded or not)
 $(function() {
+
 	// Toggle navigation
 	$('#menu-toggle').click(function() {
-		$(this).toggleClass('open');
+		// $(this).toggleClass('open');
 		if ($('body').hasClass('show-nav')) {
 			$('body').removeClass('show-nav').addClass('hide-nav');
 
 			setTimeout(function() {
 				$('body').removeClass('hide-nav');
-			}, 500);
+			}, 1700);
 
 		} else {
 			$('body').removeClass('hide-nav').addClass('show-nav');
@@ -18,11 +19,29 @@ $(function() {
 		return false;
 	});
 
+	// Word looping
+
+	i = 0;
+	skillArray = ['seo.', 'analytics.', 'automation.', 'mobile.', 'stuff.'];
+	setInterval(function() {
+		i++;
+		$('#looper').fadeOut(500, function() {
+			$(this).text(skillArray[i % skillArray.length]).fadeIn(500);
+		});
+	}, 3000);
+
 });
 
 // Toggle with hitting of ESC
 $(document).keyup(function(e) {
     if (e.keyCode == 27) {
-        $('body').removeClass('show-nav');
+		if ($('body').hasClass('show-nav')) {
+			$('body').removeClass('show-nav').addClass('hide-nav');
+
+			setTimeout(function() {
+				$('body').removeClass('hide-nav');
+			}, 1700);
+
+		}
     }
 });
