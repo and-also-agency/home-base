6344e86be161a50fab0006d4e2439ff7574af316
Sam Graim <sam@andalsoagency.com>
2017-03-29 18:35:20 -0600

    All over the place, copy, styles, etc… (Dominican)

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,10 @@
 // Nav //
+
+// to top right away
+if ( window.location.hash ) scroll(0,0);
+// void some browsers issue
+setTimeout( function() { scroll(0,0); }, 1); 
+
 // Wait for the DOM to be ready (all elements printed on page regardless if loaded or not)
 $(function() {
 
@@ -44,7 +50,27 @@ $(function() {
 	  $(this).parent().toggleClass('is-expanded');
 	  $(this).parent().siblings().removeClass('is-expanded').find('.submenu').slideUp(200);
 	  e.preventDefault();
-	});	
+	});
+
+	// Navigate and scroll
+
+    var hashLocation = false;
+    if (location.hash) {
+        hashLocation = window.location.hash;
+        setTimeout(function() {
+            hashLocation = window.location.hash;
+        }, 1); // Execute at two moments for browser compatibility reasons
+    }
+    // console.log(hashLocation);
+
+    // If we have a hash location do stuff
+    if(window.location.hash) {
+
+        // smooth scroll to the anchor id
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top + 'px'
+        }, 1500, 'swing');
+    }   
 
 });
 
